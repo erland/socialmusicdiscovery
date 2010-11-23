173ac71253175c7a48fb61aa52beab55ff8b8c63
erland@isaksson.info <erland@isaksson.info@8ba8445b-99c5-01a2-eee7-3e7c22936167>
2010-11-23 04:36:01 +0000

    Removed CreditPK and RelationPK since they looked exactly the same as the normal Credit/Relation entity classes.

--- localserver/trunk/src/smd-server/src/main/java/org/socialmusicdiscovery/server/business/model/subjective/Credit.java
+++ localserver/trunk/src/smd-server/src/main/java/org/socialmusicdiscovery/server/business/model/subjective/Credit.java
@@ -1,20 +1,24 @@
 package org.socialmusicdiscovery.server.business.model.subjective;
 
+import javax.persistence.Column;
 import javax.persistence.Id;
 import javax.persistence.IdClass;
 import javax.persistence.Table;
+import java.io.Serializable;
 
 @javax.persistence.Entity
 @Table(name = "credits")
-@IdClass(CreditPK.class)
-public class Credit {
+@IdClass(Credit.class)
+public class Credit implements Serializable {
     @Id
     private String type;
 
     @Id
+    @Column(name = "artist_person_id", length = 36)
     private String artistPersonId;
 
     @Id
+    @Column(name = "release_recording_work_id", length = 36)
     private String releaseRecordingWorkId;
 
     public String getType() {
--- localserver/trunk/src/smd-server/src/main/java/org/socialmusicdiscovery/server/business/model/subjective/Relation.java
+++ localserver/trunk/src/smd-server/src/main/java/org/socialmusicdiscovery/server/business/model/subjective/Relation.java
@@ -1,20 +1,24 @@
 package org.socialmusicdiscovery.server.business.model.subjective;
 
+import javax.persistence.Column;
 import javax.persistence.Id;
 import javax.persistence.IdClass;
 import javax.persistence.Table;
+import java.io.Serializable;
 
 @javax.persistence.Entity
 @Table(name = "relations")
-@IdClass(RelationPK.class)
-public class Relation {
+@IdClass(Relation.class)
+public class Relation implements Serializable {
     @Id
     private String type;
 
     @Id
+    @Column(name = "from_id", length = 36)
     private String fromId;
 
     @Id
+    @Column(name = "to_id", length = 36)
     private String toId;
 
     public String getType() {
