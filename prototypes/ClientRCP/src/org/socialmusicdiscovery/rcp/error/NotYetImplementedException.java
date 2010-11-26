7b588bc998c37f775f0ff43821a497773731d923
peer.torngren@gmail.com <peer.torngren@gmail.com@8ba8445b-99c5-01a2-eee7-3e7c22936167>
2010-11-26 23:29:56 +0000

    Add a popup menu to the contributor grids in ReleaseUI (only "Edit" implemented). Refactor, move out "open editor" logic to util class.

--- prototypes/ClientRCP/src/org/socialmusicdiscovery/rcp/error/NotYetImplementedException.java
+++ prototypes/ClientRCP/src/org/socialmusicdiscovery/rcp/error/NotYetImplementedException.java
@@ -5,19 +5,10 @@ public class NotYetImplementedException extends RuntimeException {
 	private static final long serialVersionUID = 2436515802934587805L;
 
 	public NotYetImplementedException() {
-		super();
+		super("Not Yet Implemented");
 	}
-
-	public NotYetImplementedException(String arg0, Throwable arg1) {
-		super(arg0, arg1);
-	}
-
 	public NotYetImplementedException(String arg0) {
-		super(arg0);
-	}
-
-	public NotYetImplementedException(Throwable arg0) {
-		super(arg0);
+		super("Not Yet Implemented: "+arg0);
 	}
 
 }
