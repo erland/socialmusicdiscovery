f804da2c7b2ae932b278a38808ecaf6582f15c5e
erland <erland@isaksson.info>
2010-12-05 17:58:42 +0000

    Added recordingSession attribute to Recording to make backward navigation possible. Added find methods to ContributorRepository and RecordingSessionRepository Corrected find method in TrackRepository Changed SearchRelationPostProcessor so it uses less memory and is usable in larger libraries.

--- localserver/trunk/src/smd-server/src/main/java/org/socialmusicdiscovery/server/business/repository/core/ContributorRepositoryImpl.java
+++ localserver/trunk/src/smd-server/src/main/java/org/socialmusicdiscovery/server/business/repository/core/ContributorRepositoryImpl.java
@@ -18,4 +18,25 @@ public class ContributorRepositoryImpl extends SMDEntityRepositoryImpl<Contribut
         return query.getResultList();
 
     }
+
+    public Collection<Contributor> findByReleaseWithRelations(String releaseId, Collection<String> mandatoryRelations, Collection<String> optionalRelations) {
+        StringBuffer queryString = new StringBuffer(200);
+        String distinct = "distinct ";
+        queryString.append("select ").append(distinct).append("e").append(" from ").append("Release as r JOIN r.contributors as e");
+        if(mandatoryRelations != null) {
+            for (String relation : mandatoryRelations) {
+                queryString.append(" JOIN FETCH ").append("e").append(".").append(relation);
+            }
+        }
+        if(optionalRelations != null) {
+            for (String relation : optionalRelations) {
+                queryString.append(" LEFT JOIN FETCH ").append("e").append(".").append(relation);
+            }
+        }
+
+        Query query = entityManager.createQuery(queryString.toString()+" WHERE r.id=:release");
+        query.setParameter("release", releaseId);
+        return query.getResultList();
+
+    }
 }
--- localserver/trunk/src/smd-server/src/main/java/org/socialmusicdiscovery/server/business/repository/core/RecordingSessionRepository.java
+++ localserver/trunk/src/smd-server/src/main/java/org/socialmusicdiscovery/server/business/repository/core/RecordingSessionRepository.java
@@ -1,9 +1,12 @@
 package org.socialmusicdiscovery.server.business.repository.core;
 
 import com.google.inject.ImplementedBy;
-import org.socialmusicdiscovery.server.business.repository.SMDEntityRepository;
 import org.socialmusicdiscovery.server.business.model.core.RecordingSession;
+import org.socialmusicdiscovery.server.business.repository.SMDEntityRepository;
+
+import java.util.Collection;
 
 @ImplementedBy(RecordingSessionRepositoryImpl.class)
 public interface RecordingSessionRepository extends SMDEntityRepository<RecordingSession> {
+    Collection<RecordingSession> findByReleaseWithRelations(String releaseId, Collection<String> mandatoryRelations, Collection<String> optionalRelations);
 }
