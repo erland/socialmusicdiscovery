c983d970d5d262c38fb76a98465c585354bfd7dc
erland <erland@isaksson.info>
2010-12-19 13:50:26 +0000

    Added support to check if a Named instance exist

--- localserver/trunk/src/smd-server/src/main/java/org/socialmusicdiscovery/server/business/logic/InjectHelper.java
+++ localserver/trunk/src/smd-server/src/main/java/org/socialmusicdiscovery/server/business/logic/InjectHelper.java
@@ -1,9 +1,6 @@
 package org.socialmusicdiscovery.server.business.logic;
 
-import com.google.inject.AbstractModule;
-import com.google.inject.Guice;
-import com.google.inject.Injector;
-import com.google.inject.Key;
+import com.google.inject.*;
 import com.google.inject.name.Names;
 
 import java.util.ArrayList;
@@ -13,6 +10,7 @@ import java.util.ServiceLoader;
 
 public class InjectHelper {
     private static Injector injector = null;
+
     static {
         Iterator<AbstractModule> moduleIterator = ServiceLoader.load(AbstractModule.class).iterator();
         List<AbstractModule> modules = new ArrayList<AbstractModule>();
@@ -24,6 +22,7 @@ public class InjectHelper {
 
     /**
      * Inject member variables in the specified object
+     *
      * @param obj Object to inject attribute values in
      */
     public static void injectMembers(Object obj) {
@@ -32,11 +31,29 @@ public class InjectHelper {
 
     /**
      * Get the instance represented by the specified name
-     * @param T The interface of the instance to get
+     *
+     * @param T    The interface of the instance to get
      * @param name The name of the instance
      * @return The instance represented by the specified name
      */
     public static <T> T instanceWithName(Class T, String name) {
-        return (T)injector.getInstance(Key.get(T, Names.named(name)));
+        return (T) injector.getInstance(Key.get(T, Names.named(name)));
+    }
+
+    /**
+     * Check if there are an instance represented by the specified name
+     *
+     * @param T    The interface of the instance to check
+     * @param name The name of the instance
+     * @return true if there is an instance with this name
+     */
+    public static <T> boolean existsWithName(Class T, String name) {
+        List<Binding<T>> bindings = injector.findBindingsByType(TypeLiteral.get(T));
+        for (Binding<T> binding : bindings) {
+            if (binding.getKey().equals(Key.get(T, Names.named(name)))) {
+                return true;
+            }
+        }
+        return false;
     }
 }
