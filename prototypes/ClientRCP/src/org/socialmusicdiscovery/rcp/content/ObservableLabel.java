e0eee538c41ba4bdef31d1fd5026c48ff726ab84
peer.torngren <peer.torngren@gmail.com>
2011-01-06 20:25:25 +0000

    - Refactor: delete server jar from lib, assume .jar is externally built and copied into new folder lib.imported - Update to match most recent version of server jar (add ObservableLabel)

---
+++ prototypes/ClientRCP/src/org/socialmusicdiscovery/rcp/content/ObservableLabel.java
@@ -0,0 +1,8 @@
+package org.socialmusicdiscovery.rcp.content;
+
+import org.socialmusicdiscovery.server.business.model.core.Label;
+
+public class ObservableLabel extends AbstractObservableEntity<Label> implements Label {
+
+
+}
