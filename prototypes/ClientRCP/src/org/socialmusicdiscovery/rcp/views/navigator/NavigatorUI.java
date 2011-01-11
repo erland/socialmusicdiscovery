d1c1b594cfd997913915414d3fd0c479910ad14e
peer.torngren <peer.torngren@gmail.com>
2011-01-11 02:05:11 +0000

    Use a preliminary ExpandBar in navigator UI. Needs more work to do proper layout (grab all space, resize with parent).

--- prototypes/ClientRCP/src/org/socialmusicdiscovery/rcp/views/navigator/NavigatorUI.java
+++ prototypes/ClientRCP/src/org/socialmusicdiscovery/rcp/views/navigator/NavigatorUI.java
@@ -5,25 +5,102 @@ import org.eclipse.jface.databinding.viewers.ObservableListTreeContentProvider;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.viewers.ViewerSorter;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.ExpandBar;
+import org.eclipse.swt.widgets.ExpandItem;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Tree;
 import org.socialmusicdiscovery.rcp.content.DataSource;
 import org.socialmusicdiscovery.rcp.views.util.DefaultLabelProvider;
 
 public class NavigatorUI extends Composite {
 
 	private TreeViewer treeViewer;
+	private ExpandBar expandBar;
+	private ExpandItem itemTree;
+	private ExpandItem itemOther;
+	private Composite otherArea;
+	private Label lblplaceholder;
 
 	public NavigatorUI(Composite parent, int style) {
 		super(parent, style);
-		setLayout(new FillLayout(SWT.HORIZONTAL));
+		GridLayout gridLayout = new GridLayout(1, false);
+		gridLayout.verticalSpacing = 0;
+		gridLayout.horizontalSpacing = 0;
+		gridLayout.marginHeight = 0;
+		gridLayout.marginWidth = 0;
+		setLayout(gridLayout);
 		
-		treeViewer = new TreeViewer(this, SWT.BORDER);
+		expandBar = new ExpandBar(this, SWT.NONE);
+		expandBar.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+		
+		itemTree = new ExpandItem(expandBar, SWT.NONE);
+		itemTree.setExpanded(true);
+		itemTree.setText("Basic Tree");
+		itemTree.setHeight(300);
+		
+		treeViewer = new TreeViewer(expandBar, SWT.BORDER);
+		Tree tree = treeViewer.getTree();
+		itemTree.setControl(tree);
 		treeViewer.setSorter(new ViewerSorter());
 		
 		ObservableListTreeContentProvider contentProvider = new ObservableListTreeContentProvider(new NavigatorListFactory(), new NavigatorStructureAdvisor());
 		treeViewer.setContentProvider(contentProvider);
 		treeViewer.setLabelProvider(new DefaultLabelProvider());
+		itemTree.setHeight(-1);
+		
+		itemOther = new ExpandItem(expandBar, SWT.NONE);
+		itemOther.setText("Other View");
+		
+		otherArea = new Composite(expandBar, SWT.NONE);
+		itemOther.setControl(otherArea);
+		otherArea.setLayout(new GridLayout(1, false));
+		
+		lblplaceholder = new Label(otherArea, SWT.NONE);
+		lblplaceholder.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, true, 1, 1));
+		lblplaceholder.setBounds(0, 0, 49, 13);
+		lblplaceholder.setText("(placeholder)");
+		Label todoLabel = new Label(otherArea, SWT.NONE);
+		todoLabel.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1));
+		todoLabel.setText("TODO: fix layout");
+		itemOther.setHeight(itemOther.getControl().computeSize(SWT.DEFAULT, SWT.DEFAULT).y);
+		
+	  // TODO add listeners to recalculate heights?
+	  // see http://dev.eclipse.org/viewcvs/viewvc.cgi/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet343.java?view=co
+	  // see http://stackoverflow.com/questions/586414/why-does-an-swt-composite-sometimes-require-a-call-to-resize-to-layout-correctl
+	  // see http://www.eclipsezone.com/eclipse/forums/t76814.html
+//		you can use ExpandItem#setHeight to control how much space the item takes
+//		when it is expand. You can actually change this value on the fly when
+//		other item expands or the widget resizes.
+//		you can use ExpandItem#getHeaderHeight() to get the height of the title.
+//
+//		This snippet forces the item[0] to take up all the available space in the
+//		parent:
+//
+//		Listener listener = new Listener() {
+//		public void handleEvent(Event e) {
+//		display.asyncExec(new Runnable() {
+//		public void run () {
+//		ExpandItem[] items = expandBar.getItems();
+//		Rectangle area = expandBar.getClientArea();
+//		int spacing = expandBar.getSpacing();
+//		// area.width -= 2*spacing;
+//		int header0 = items[0].getHeaderHeight();
+//		int header1 = items[1].getHeaderHeight();
+//		area.height -= (items.length + 1)*spacing + header0 + header1;
+//		if (items[1].getExpanded()) {
+//		area.height -= items[1].getHeight();// + spacing;
+//		}
+//		items[0].setHeight(area.height);
+//		}
+//		});
+//		}
+//		};
+//		expandBar.addListener(SWT.Resize, listener);
+//		expandBar.addListener(SWT.Expand, listener);
+//		expandBar.addListener(SWT.Collapse, listener);
 
 	}
 
