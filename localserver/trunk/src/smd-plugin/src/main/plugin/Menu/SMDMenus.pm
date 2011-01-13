9e00349942d28b94ebc8bed9cf73402c6511128c
erland <erland@isaksson.info>
2011-01-13 06:26:28 +0000

    Changed so it uses browse menus defined in SMD Server instead of hard coding them in the plugin

--- localserver/trunk/src/smd-plugin/src/main/plugin/Menu/SMDMenus.pm
+++ localserver/trunk/src/smd-plugin/src/main/plugin/Menu/SMDMenus.pm
@@ -49,7 +49,7 @@ use Plugins::SocialMusicDiscovery::MenuAPI::BrowseLibrary;
 
 my $serverPrefs = preferences('server');
 my $prefs = preferences('plugin.socialmusicdiscovery');
-my $log = logger('database.info');
+my $log = logger('plugin.socialmusicdiscovery');
 
 my %nodeFilters;
 
@@ -58,41 +58,17 @@ sub init {
 	
 	main::DEBUGLOG && $log->is_debug && $log->debug('init');
 	
-	Plugins::SocialMusicDiscovery::MenuAPI::BrowseLibrary->setFeedForMode('tracks',\&_tracks);
-
 	my @topLevel = (
 		{
 			type         => 'link',
-			name         => 'PLUGIN_SOCIALMUSICDISCOVERY_BROWSE_BY_ARTIST',
-			params       => {mode => 'artists'},
-			feed         => \&_artists,
+			name         => 'PLUGIN_SOCIALMUSICDISCOVERY',
+			params       => {mode => 'smd'},
+			feed         => \&_smd,
 			icon         => 'html/images/artists.png',
-			homeMenuText => 'PLUGIN_SOCIALMUSICDISCOVERY_BROWSE_ARTISTS',
+			homeMenuText => 'PLUGIN_SOCIALMUSICDISCOVERY',
 			condition    => \&Slim::Schema::hasLibrary,
-			id           => 'SMDArtists',
+			id           => 'SMD',
 			weight       => 81,
-		},
-		{
-			type         => 'link',
-			name         => 'PLUGIN_SOCIALMUSICDISCOVERY_BROWSE_BY_RELEASE',
-			params       => {mode => 'releases'},
-			feed         => \&_releases,
-			icon         => 'html/images/albums.png',
-			homeMenuText => 'PLUGIN_SOCIALMUSICDISCOVERY_BROWSE_RELEASES',
-			condition    => \&Slim::Schema::hasLibrary,
-			id           => 'SMDReleases',
-			weight       => 82,
-		},
-		{
-			type         => 'link',
-			name         => 'PLUGIN_SOCIALMUSICDISCOVERY_BROWSE_BY_CLASSIFICATION',
-			params       => {mode => 'classifications'},
-			feed         => \&_classifications,
-			icon         => 'html/images/genres.png',
-			homeMenuText => 'PLUGIN_SOCIALMUSICDISCOVERY_BROWSE_CLASSIFICATIONS',
-			condition    => \&Slim::Schema::hasLibrary,
-			id           => 'SMDClassifications',
-			weight       => 83,
 		},
 	);
 	
@@ -105,7 +81,6 @@ sub _generic {
 	my ($client,
 		$callback,
 		$args,
-		$query,
 		$criterias,
 		$resultsFunc
 	) = @_;
@@ -117,29 +92,23 @@ sub _generic {
 	my $quantity = $args->{'quantity'} || $args->{'params'}->{'_quantity'} || 10000000; #Not sure why this is needed, might be some change in the "onebrowser" branch requiring it
 	
 	my $params = $args->{'params'};
-	for my $key (keys %$params) {
-		foreach (qw(Artist Release Classification Track)) {
-			if($_ eq $key) {
-				push @$criterias,$key.':'.$params->{$key};
-				last;
-			}
-		}
+	my $path = "";
+	if(defined($params->{'path'})) {
+		$path = $params->{'path'};
 	}
 
-	main::INFOLOG && $log->is_info && $log->info("$query ($index, $quantity): tags ->", join(', ', @$criterias));
+	main::INFOLOG && $log->is_info && $log->info("$path ($index, $quantity): tags ->", join(', ', @$criterias));
 	
 	my $http = Slim::Networking::SimpleAsyncHTTP->new(\&_genericReply, \&_genericError, {
+		path => $path,
                 client => $client, 
                 callback => $callback, 
                 resultsFunc => $resultsFunc, 
         });
 
-	my $url = "http://".$hostname.":".$port."/browse/".$query."?";
-	if(defined($criterias) && scalar(@$criterias)>0) {
-		$url .= "&criteria=".join('&criteria=', @$criterias);
-	}
+	my $url = "http://".$hostname.":".$port."/browse/library".$path;
 	if($index>0 || $quantity<1000000) {
-		$url .= "&offset=".$index."&size=".$quantity;
+		$url .= "?offset=".$index."&size=".$quantity;
 	}
 	$log->info("Getting data using: ".$url);
 	$http->get($url);
@@ -163,7 +132,7 @@ sub _genericReply {
 	my $content = $http->content();
 
 	my $jsonResult = JSON::XS::decode_json($content);
-	my ($result, $extraitems) = $params->{'resultsFunc'}->($jsonResult);
+	my ($result, $extraitems) = $params->{'resultsFunc'}->($params->{'path'}, $jsonResult);
 	$result->{'offset'} = $jsonResult->{'offset'};
 	$result->{'count'} = $jsonResult->{'size'};
 	$result->{'total'} = $jsonResult->{'totalSize'};
@@ -181,60 +150,8 @@ sub _tagsToParams {
 	return \%p;
 }
 
-sub _classifications {
-	my ($client, $callback, $args, $pt) = @_;
-	my @searchTags = $pt->{'searchTags'} ? @{$pt->{'searchTags'}} : ();
 
-	_generic($client, $callback, $args, 'Classification', [@searchTags],
-		sub {
-			my $results = shift;
-			my @empty = ();
-			my $items = \@empty;
-			foreach (@{$results->{'items'}}) {
-				my $item = {
-					'id' => $_->{'item'}->{'id'},
-					'name' => $_->{'item'}->{'name'},
-					'type' => 'playlist',
-					'playlist' => \&_tracks,
-					'url' => \&_artists,
-					'passthrough' => [ { searchTags => [@searchTags, "Genre:" . $_->{'item'}->{'id'}] } ],
-					'favorites_url' => 'smd:classification='.$_->{'item'}->{'id'},
-				};
-				push @$items,$item;
-			}
-			
-			my %actions = (
-				allAvailableActionsDefined => 1,
-				commonVariables	=> ['Classification' => 'id'],
-#				info => {
-#					command     => ['genreinfo', 'items'],
-#				},
-				items => {
-					command     => [Plugins::SocialMusicDiscovery::MenuAPI::BrowseLibrary::BROWSELIBRARY, 'items'],
-					fixedParams => {mode => 'artists'},
-				},
-#				play => {
-#					command     => ['playlistcontrol'],
-#					fixedParams => {cmd => 'load'},
-#				},
-#				add => {
-#					command     => ['playlistcontrol'],
-#					fixedParams => {cmd => 'add'},
-#				},
-#				insert => {
-#					command     => ['playlistcontrol'],
-#					fixedParams => {cmd => 'insert'},
-#				},
-			);
-#			$actions{'playall'} = $actions{'play'};
-#			$actions{'addall'} = $actions{'add'};
-			
-			return {items => $items, actions => \%actions, sorted => 1}, undef;
-		},
-	);
-}
-
-sub _artists {
+sub _smd {
 	my ($client, $callback, $args, $pt) = @_;
 	my @searchTags = $pt->{'searchTags'} ? @{$pt->{'searchTags'}} : ();
 	my $search     = $pt->{'search'};
@@ -242,45 +159,39 @@ sub _artists {
 	if (!$search && !scalar @searchTags && $args->{'search'}) {
 		$search = $args->{'search'};
 	}
-	
-	my @ptSearchTags;
-	if ($serverPrefs->get('noGenreFilter')) {
-		@ptSearchTags = grep {$_ !~ /^Classification:/} @searchTags;
-	} else {
-		@ptSearchTags = @searchTags;
-	}
 
-	_generic($client, $callback, $args, 'Artist', 
+	_generic($client, $callback, $args, 
 		[@searchTags],
 		sub {
+			my $path = shift;
 			my $results = shift;
 			my @empty = ();
 			my $items = \@empty;
 			foreach (@{$results->{'items'}}) {
 				my $item = {
-					'id' => $_->{'item'}->{'id'},
-					'name' => $_->{'item'}->{'name'},
+					'id' => $path."/".$_->{'id'},
+					'name' => $_->{'name'},
 					'type' => 'playlist',
 					'playlist' => \&_tracks,
-					'url' => \&_releases,
-					'passthrough' => [ { searchTags => [@ptSearchTags, "Artist:" . $_->{'item'}->{'id'}] } ],
-					'favorites_url' => 'smd:artist='.$_->{'item'}->{'id'},
+					'url' => \&_smd,
+					'passthrough' => [ { searchTags => [@searchTags, "path:" . $path."/".$_->{'id'}] } ],
+					'favorites_url' => 'smd:object='.$_->{'id'},
 				};
 				push @$items,$item;
 			}
 			my $extra;
 			
-			my $params = _tagsToParams(\@ptSearchTags);
+			my $params = _tagsToParams(\@searchTags);
 			my %actions = (
 				allAvailableActionsDefined => 1,
-				commonVariables	=> ['Artist' => 'id'],
+				commonVariables	=> ['path' => 'id'],
 #				info => {
 #					command     => ['artistinfo', 'items'],
 #				},
 				items => {
 					command     => [Plugins::SocialMusicDiscovery::MenuAPI::BrowseLibrary::BROWSELIBRARY, 'items'],
 					fixedParams => {
-						mode       => 'releases',
+						mode       => 'smd',
 						%{&_tagsToParams(\@searchTags)},
 					},
 				},
@@ -305,155 +216,19 @@ sub _artists {
 	);
 }
 
-sub _releases {
-	my ($client, $callback, $args, $pt) = @_;
-	my @searchTags = $pt->{'searchTags'} ? @{$pt->{'searchTags'}} : ();
-	my $search     = $pt->{'search'};
-
-	if (!$search && !scalar @searchTags && $args->{'search'}) {
-		$search = $args->{'search'};
-	}
-	
-	my @ptSearchTags;
-	if ($serverPrefs->get('noGenreFilter')) {
-		@ptSearchTags = grep {$_ !~ /^Classification:/} @searchTags;
-	} else {
-		@ptSearchTags = @searchTags;
-	}
-
-	_generic($client, $callback, $args, 'Release', 
-		[@searchTags],
-		sub {
-			my $results = shift;
-			my @empty = ();
-			my $items = \@empty;
-			foreach (@{$results->{'items'}}) {
-				my $item = {
-					'id' => $_->{'item'}->{'id'},
-					'name' => $_->{'item'}->{'name'},
-					'type' => 'playlist',
-					'playlist' => \&_tracks,
-					'url' => \&_tracks,
-					'passthrough' => [ { searchTags => [@ptSearchTags, "Release:" . $_->{'item'}->{'id'}] } ],
-					'favorites_url' => 'smd:release='.$_->{'item'}->{'id'},
-				};
-				push @$items,$item;
-			}
-			my $extra;
-			
-			my $params = _tagsToParams(\@ptSearchTags);
-			my %actions = (
-				allAvailableActionsDefined => 1,
-				commonVariables	=> ['Release' => 'id'],
-#				info => {
-#					command     => ['albuminfo', 'items'],
-#				},
-				items => {
-					command     => [Plugins::SocialMusicDiscovery::MenuAPI::BrowseLibrary::BROWSELIBRARY, 'items'],
-					fixedParams => {
-						mode       => 'tracks',
-						%{&_tagsToParams(\@searchTags)},
-					},
-				},
-#				play => {
-#					command     => ['playlistcontrol'],
-#					fixedParams => {cmd => 'load', %$params},
-#				},
-#				add => {
-#					command     => ['playlistcontrol'],
-#					fixedParams => {cmd => 'add', %$params},
-#				},
-#				insert => {
-#					command     => ['playlistcontrol'],
-#					fixedParams => {cmd => 'insert', %$params},
-#				},
-			);
-#			$actions{'playall'} = $actions{'play'};
-#			$actions{'addall'} = $actions{'add'};
-			
-			return {items => $items, actions => \%actions, sorted => 1}, $extra;
-		},
-	);
-}
 
 sub _tracks {
 	my ($client, $callback, $args, $pt) = @_;
 	my @searchTags = $pt->{'searchTags'} ? @{$pt->{'searchTags'}} : ();
-#	my $sort       = $pt->{'sort'} || 'sort:tracknum';
 	my $search     = $pt->{'search'};
-	my $offset     = $args->{'index'} || 0;
-	
+
 	if (!$search && !scalar @searchTags && $args->{'search'}) {
 		$search = $args->{'search'};
 	}
 
-	_generic($client, $callback, $args, 'Track',
-		[@searchTags],
-		sub {
-			my $results = shift;
-			my @empty = ();
-			my $items = \@empty;
-			foreach (@{$results->{'items'}}) {
-				my $tracknum = $_->{'item'}->{'number'} ? $_->{'item'}->{'number'} . '. ' : '';
-				if(defined($_->{'item'}->{'medium'}->{'number'})) {
-					$tracknum = $_->{'item'}->{'medium'}->{'number'}."-".$tracknum;
-				}elsif(defined($_->{'item'}->{'medium'}->{'name'})) {
-					$tracknum = $_->{'item'}->{'medium'}->{'name'}."-".$tracknum;
-				}
-				my $tracktitle = $_->{'item'}->{'recording'}->{'name'} ? $_->{'item'}->{'recording'}->{'name'} : undef;
-				if(!defined($tracktitle) && defined($_->{'item'}->{'recording'}->{'work'}->{'name'})) {
-					$tracktitle = $_->{'item'}->{'recording'}->{'work'}->{'name'};
-					if(defined($_->{'item'}->{'recording'}->{'work'}->{'parent'})) {
-						$tracktitle = $_->{'item'}->{'recording'}->{'work'}->{'parent'}->{'name'}.': '.$tracktitle;
-					}
-				}
-				my $item = {
-					'id' => $_->{'item'}->{'id'},
-					'name' => $tracknum.$tracktitle,
-					'type' => 'audio',
-					'playlist' => \&_tracks,
-					'playall'       => 1,
-					'play_index'    => $offset++,
-				};
-				push @$items,$item;
-			}
-			
-			my %actions = (
-				commonVariables	=> ['Track' => 'id'],
-				allAvailableActionsDefined => 1,
-				
-#				info => {
-#					command     => ['trackinfo', 'items'],
-#				},
-#				play => {
-#					command     => ['playlistcontrol'],
-#					fixedParams => {cmd => 'load'},
-#				},
-#				add => {
-#					command     => ['playlistcontrol'],
-#					fixedParams => {cmd => 'add'},
-#				},
-#				insert => {
-#					command     => ['playlistcontrol'],
-#					fixedParams => {cmd => 'insert'},
-#				},
-			);
-#			$actions{'items'} = $actions{'info'};	# XXX, not sure about this, probably harmless but unnecessary
-
-#			$actions{'playall'} = {
-#				command     => ['playlistcontrol'],
-#				fixedParams => {cmd => 'load', %{&_tagsToParams([@searchTags, $sort])}},
-#				variables	=> [play_index => 'play_index'],
-#			};
-#			$actions{'addall'} = {
-#				command     => ['playlistcontrol'],
-#				variables	=> [],
-#				fixedParams => {cmd => 'add', %{&_tagsToParams([@searchTags, $sort])}},
-#			};
-			
-			return {items => $items, actions => \%actions, sorted => 0, albumData => undef, cover => undef}, undef;
-		},
-	);
+	#TODO: Implement this function, at the moment it's just a stupid placeholder
+	my $result = {};
+	$callback->($result);
 }
 
 1;
