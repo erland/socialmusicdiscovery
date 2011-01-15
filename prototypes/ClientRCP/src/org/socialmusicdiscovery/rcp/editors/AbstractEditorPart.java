e4ebcc96855efedb8e9d3ad53f287b3257fa7c6f
peer.torngren <peer.torngren@gmail.com>
2011-01-15 21:41:25 +0000

    Implement basic support for monitoring and rendering dirty status. - Changes to regular properties now set dirty status and fire events. - Both editor title and viewer labels render dirty status - Default save operation is triggered when closing dirty editor, or when exiting client with dirty editors.
    TODO 
    - The actual save (to server DB) is *NOT* implemented
    - There is no "Save" command or menu
    - Viewer labels render dirty very simple (with a trailing *) - would like a bit more sophistication here (color/font/image?)

--- prototypes/ClientRCP/src/org/socialmusicdiscovery/rcp/editors/AbstractEditorPart.java
+++ prototypes/ClientRCP/src/org/socialmusicdiscovery/rcp/editors/AbstractEditorPart.java
@@ -1,8 +1,12 @@
 package org.socialmusicdiscovery.rcp.editors;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IEditorInput;
@@ -10,13 +14,33 @@ import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.part.EditorPart;
 import org.socialmusicdiscovery.rcp.content.AbstractObservableEntity;
+import org.socialmusicdiscovery.rcp.content.ObservableEntity;
 import org.socialmusicdiscovery.rcp.util.TextUtil;
 import org.socialmusicdiscovery.rcp.util.ViewerUtil;
 import org.socialmusicdiscovery.rcp.views.util.AbstractComposite;
 
 public abstract class AbstractEditorPart<T extends AbstractObservableEntity, U extends AbstractComposite<T>> extends EditorPart {
 
+	private class MyDirtyStatusListener implements PropertyChangeListener {
+		private final T observed;
+
+		private MyDirtyStatusListener(T observed) {
+			this.observed = observed;
+			this.observed.addPropertyChangeListener(ObservableEntity.PROP_dirty, this);
+		}
+
+		public void dispose() {
+			observed.removePropertyChangeListener(ObservableEntity.PROP_dirty, this);
+		}
+
+		@Override
+		public void propertyChange(PropertyChangeEvent evt) {
+	       firePropertyChange(PROP_DIRTY);
+	    }
+	}
+
 	private U ui;
+	private MyDirtyStatusListener dirtyStatusListener;
 
 	public AbstractEditorPart() {
 	}
@@ -61,32 +85,53 @@ public abstract class AbstractEditorPart<T extends AbstractObservableEntity, U e
 	}
 	@Override
 	public void doSave(IProgressMonitor monitor) {
-		// Do the Save operation
+		monitor.beginTask("Saving "+getPartName(), -1);
+		MessageDialog.openWarning(getSite().getShell(), "Not Yet Implemented",
+				"Sorry, save operation is not yet implemented. Will fake a successful save, but nothing will be written to database.");
+		getEntity().setDirty(false);
 	}
 
+	/**
+	 * @see #isSaveAsAllowed()
+	 */
 	@Override
 	public void doSaveAs() {
-		// Do the Save As operation
+		throw new UnsupportedOperationException();
 	}
-
-	@Override
-	public void init(IEditorSite site, IEditorInput input) throws PartInitException {
-		setSite(site);
-		setInput(input);
-	}
-
-	@Override
-	public boolean isDirty() {
-		return false;
-	}
-
+	
 	@Override
 	public boolean isSaveAsAllowed() {
 		return false;
 	}
 
+	@SuppressWarnings("unchecked")
+	@Override
+	public void init(IEditorSite site, IEditorInput input) throws PartInitException {
+		setSite(site);
+		setInput(input);
+		hookDirtyStatusListener((T) input);
+	}
+
+	private void hookDirtyStatusListener(T entity) {
+		if (dirtyStatusListener!=null) {
+			dirtyStatusListener.dispose();
+		}
+		dirtyStatusListener = new MyDirtyStatusListener(entity);
+	}
+
+	@Override
+	public boolean isDirty() {
+		ObservableEntity entity = getEntity();
+		return entity!=null && entity.isDirty();
+	}
+
 	protected U getUI() {
 		return ui;
+	}
+
+	@SuppressWarnings("unchecked")
+	protected T getEntity() {
+		return (T) getEditorInput();
 	}
 
 	protected void hookContextMenus(Viewer... viewers) {
@@ -95,5 +140,18 @@ public abstract class AbstractEditorPart<T extends AbstractObservableEntity, U e
 		}
 	}
 
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		if (dirtyStatusListener!=null) {
+			dirtyStatusListener.dispose();
+		}
+		super.dispose();
+	}
 
 }
