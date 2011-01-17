18443cd751b05b2146489f87f663fe58c88bddad
peer.torngren@gmail.com <peer.torngren@gmail.com@8ba8445b-99c5-01a2-eee7-3e7c22936167>
2011-01-17 21:49:16 +0000

    Implement Save and SaveAll commands

--- prototypes/ClientRCP/src/org/socialmusicdiscovery/rcp/ApplicationActionBarAdvisor.java
+++ prototypes/ClientRCP/src/org/socialmusicdiscovery/rcp/ApplicationActionBarAdvisor.java
@@ -11,6 +11,8 @@ public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
 
     private IWorkbenchAction introAction;
 	private IWorkbenchAction preferenceAction;
+	private IWorkbenchAction saveAction;
+	private IWorkbenchAction saveAllAction;
     
 	public ApplicationActionBarAdvisor(IActionBarConfigurer configurer) {
 		super(configurer);
@@ -18,13 +20,24 @@ public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
 
 	protected void makeActions(IWorkbenchWindow window) {
 		// built-in commands/actions needs to be created here? Cannot create thru extension point?
-		// see http://www.eclipsezone.com/eclipse/forums/t113363.html
+		// http://www.eclipsezone.com/eclipse/forums/t113363.html
+		// http://dev.eclipse.org/newslists/news.eclipse.platform.rcp/msg36330.html
 
 		preferenceAction = ActionFactory.PREFERENCES.create(window);
 		register(preferenceAction);
 
 		introAction = ActionFactory.INTRO.create(window);
 		register(introAction);
+		
+        saveAction = ActionFactory.SAVE.create(window);
+        register(saveAction);
+
+        saveAllAction = ActionFactory.SAVE_ALL.create(window);
+        register(saveAllAction);
+
+
+
+
 	}
 
 	protected void fillMenuBar(IMenuManager menuBar) {
