a7af8dfa654bf5e81bb625f45b60b25398cf6db0
peer.torngren <peer.torngren@gmail.com>
2011-02-08 06:26:53 +0000

    Fix bad import (triggered by javadoc)

--- prototypes/ClientRCP/src/org/socialmusicdiscovery/rcp/views/util/LabelProviderFactory.java
+++ prototypes/ClientRCP/src/org/socialmusicdiscovery/rcp/views/util/LabelProviderFactory.java
@@ -1,7 +1,5 @@
 package org.socialmusicdiscovery.rcp.views.util;
 
-import javassist.tools.web.Viewer;
-
 import org.eclipse.core.databinding.beans.BeanProperties;
 import org.eclipse.core.databinding.observable.map.IObservableMap;
 import org.eclipse.core.databinding.observable.set.IObservableSet;
@@ -10,6 +8,7 @@ import org.eclipse.core.databinding.property.value.IValueProperty;
 import org.eclipse.jface.databinding.viewers.ObservableListTreeContentProvider;
 import org.eclipse.jface.databinding.viewers.ObservableMapLabelProvider;
 import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.Viewer;
 import org.socialmusicdiscovery.rcp.content.ObservableEntity;
 
 /**
