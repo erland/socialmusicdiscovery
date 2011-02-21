592c526c752c5a513b941af3beee7c91a2072fc5
peer.torngren <peer.torngren@gmail.com>
2011-02-21 17:27:42 +0000

    Remove project in favor of Maven-based variant (ClientRCP_maven).

--- prototypes/ClientRCP/src/org/socialmusicdiscovery/rcp/content/ObservablePerson.java
+++
@@ -1,7 +0,0 @@
-package org.socialmusicdiscovery.rcp.content;
-
-import org.socialmusicdiscovery.server.business.model.core.Person;
-
-public class ObservablePerson extends AbstractObservableEntity<Person> implements Person {
-
-}
