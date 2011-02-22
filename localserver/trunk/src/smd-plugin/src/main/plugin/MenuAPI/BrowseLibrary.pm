646c6b23225f0f37f69cebc8cc879102befc7ab2
erland@isaksson.info <erland@isaksson.info@8ba8445b-99c5-01a2-eee7-3e7c22936167>
2011-02-22 05:02:14 +0000

    Added copyright header to all perl files

--- localserver/trunk/src/smd-plugin/src/main/plugin/MenuAPI/BrowseLibrary.pm
+++ localserver/trunk/src/smd-plugin/src/main/plugin/MenuAPI/BrowseLibrary.pm
@@ -1,4 +1,4 @@
-#   Copyright 2010, Social Music Discovery project
+#   Copyright 2010-2011, Social Music Discovery project
 #   All rights reserved.
 #
 #   Redistribution and use in source and binary forms, with or without
