3cd32e7801002de981abbbb0e891486a02858a25
peer.torngren <peer.torngren@gmail.com>
2011-03-12 13:39:58 +0000

    RCP: Refactor; move ContributorPanel to new package

--- prototypes/ClientRCP_maven/smd-rcp-client-plugin/src/org/socialmusicdiscovery/rcp/editors/widgets/ContributorPanel.java
+++ prototypes/ClientRCP_maven/smd-rcp-client-plugin/src/org/socialmusicdiscovery/rcp/editors/widgets/ContributorPanel.java
@@ -25,7 +25,7 @@
  *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.socialmusicdiscovery.rcp.editors.release;
+package org.socialmusicdiscovery.rcp.editors.widgets;
 
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.nebula.jface.gridviewer.GridTableViewer;
