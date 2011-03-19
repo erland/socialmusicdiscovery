8e1ff3b310375a98cca6b03a6c5a0bc263f09c80
peer.torngren@gmail.com <peer.torngren@gmail.com@8ba8445b-99c5-01a2-eee7-3e7c22936167>
2011-03-19 15:36:46 +0000

    RCP: Update TextUtilTest and fix bug in TextUtil

--- prototypes/ClientRCP_maven/smd-rcp-client-plugin-tests/src/org/socialmusicdiscovery/rcp/util/TextUtilTest.java
+++ prototypes/ClientRCP_maven/smd-rcp-client-plugin-tests/src/org/socialmusicdiscovery/rcp/util/TextUtilTest.java
@@ -1,21 +1,23 @@
 package org.socialmusicdiscovery.rcp.util;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class TextUtilTest {
 	@Test
 	public void testGetText() {
 		String result = TextUtil.getText(String.class);
-		assert result.equals("String");
+		assertEquals("String", result);
 	}
 	
 	@Test
 	public void testGetShortText() {
 		String original = "A really long text which will be very hard to fit in the user interface";
 		String result = TextUtil.getShortText(original);
-		assert result.length()<original.length();
-		assert result.contains("...");
-		assert result.startsWith(original.substring(0,10));
-		assert result.endsWith(original.substring(original.length()-10));
+		assertTrue("Not shorter", result.length()<original.length());
+		assertTrue("No dots", result.contains("...")); // won't work if/when we localize strings 
+		assertTrue("Bad start: "+result, result.startsWith(original.substring(0,5)));
+		assertTrue("Bad end: "+result, result.endsWith(original.substring(original.length()-5)));
 	}
 }
