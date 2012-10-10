373b7eeeba8385a1c0acefc9159c7f9330d6a2f7
vrobin@gmail.com <vrobin@gmail.com@8ba8445b-99c5-01a2-eee7-3e7c22936167>
2012-10-10 20:16:54 +0000

    Antlr parsing unit test commented until there's something more functional to test.

--- localserver/trunk/src/smd-server/src/test/java/org/socialmusicdiscovery/server/plugins/upnp/SearchCriteriaParserTest.java
+++ localserver/trunk/src/smd-server/src/test/java/org/socialmusicdiscovery/server/plugins/upnp/SearchCriteriaParserTest.java
@@ -1,10 +1,10 @@
 package org.socialmusicdiscovery.server.plugins.upnp;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-
-import org.antlr.runtime.*;
-import org.socialmusicdiscovery.server.support.format.antlr.*;
+//import java.io.ByteArrayInputStream;
+//import java.io.IOException;
+//
+//import org.antlr.runtime.*;
+//import org.socialmusicdiscovery.server.support.format.antlr.*;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
@@ -23,28 +23,28 @@ public class SearchCriteriaParserTest {
 
   @Test
   public void basicParsing() {
-		// Create an input character stream from standard in
-		ANTLRInputStream input;
-		ExprParser parser;
-		try {
-			//input = new ANTLRInputStream(System.in);
-			input = new ANTLRInputStream(new  ByteArrayInputStream("((1+4)*(11*5+3)+n)\n".getBytes()));
-		// Create an ExprLexer that feeds from that stream
-		ExprLexer lexer = new ExprLexer(input);
-		// Create a stream of tokens fed by the lexer
-		CommonTokenStream tokens = new CommonTokenStream(lexer);
-		// Create a parser that feeds off the token stream
-		parser = new ExprParser(tokens);
-		System.out.println("before prog");
-		parser.prog();
-		System.out.println("test");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (RecognitionException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		// Create an input character stream from standard in
+//		ANTLRInputStream input;
+//		ExprParser parser;
+//		try {
+//			//input = new ANTLRInputStream(System.in);
+//			input = new ANTLRInputStream(new  ByteArrayInputStream("((1+4)*(11*5+3)+n)\n".getBytes()));
+//		// Create an ExprLexer that feeds from that stream
+//		ExprLexer lexer = new ExprLexer(input);
+//		// Create a stream of tokens fed by the lexer
+//		CommonTokenStream tokens = new CommonTokenStream(lexer);
+//		// Create a parser that feeds off the token stream
+//		parser = new ExprParser(tokens);
+//		System.out.println("before prog");
+//		parser.prog();
+//		System.out.println("test");
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (RecognitionException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
   }
   @BeforeMethod
   public void beforeMethod() {
